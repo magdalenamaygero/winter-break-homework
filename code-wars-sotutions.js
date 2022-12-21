1c1325885e4de7ca40854b3b169b1d447f3cb9d6
magdalenamaygero <magdalenamgero@gmail.com>
2022-12-21 20:51:57 +0000

    middle

--- code-wars-sotutions.js
+++ code-wars-sotutions.js
@@ -38,7 +38,7 @@ function likes(names) {
 P - user inputs arary of names, we return string depending on the amount of names
     if the amount of names is greater than 3, print 2 names and a sentence
     
-E - ["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+E - ["Max", "John", "Mark"]   -->  "Max, John and Mark like this"
     ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
     
 D - arrays, string
@@ -47,4 +47,50 @@ A - if length = 0, return "no one likes this"
     ... = 2, `${names[0]} and ${names[1]} like this`
     ... = 3, `${names[0]}, ${names[1]} and ${names[2]} like this`
     else :  = 4, `${names[0]}, ${names[1]} and ${names.length - 2} others like this` 
+*/
+
+function findUniq(arr) {
+  arr.sort((one, two) => one-two);
+  if (arr[0] !== arr[1]) {
+    return arr[0]
+  } else {
+    return arr[arr.length - 1]
+  }
+};
+
+
+function findUniq(arr) {
+  arr.sort((one, two) => one-two);
+  return (arr[0] !== arr[1]) ? arr[0] :
+  arr[arr.length - 1]
+}
+
+
+/*
+P - input array of integers, output the (unique) number
+E - findUniq([ 1, 1, 1, 2, 1, 1 ]) === 2
+    findUniq([ 0, 0, 0.55, 0, 0 ]) === 0.55
+D - arrays, integers
+A - arr.sort();
+    if (arr[0] !== arr[1]) {
+    } return arr[0]
+    } else {
+    return arr[arr.length - 1]
+    }
+*/
+
+function findOutlier(integers){
+  const even = integers.filter(element => element % 2 === 0)
+  const odd = integers.filter(element => element % 2 !== 0)
+  if (even.length === 1) return even[0]
+  else return odd[0]
+}
+
+/*
+P - given array of nums (all even or all odd, except 1)
+E - [2, 4, 0, 100, 4, 11, 2602, 36]
+  Should return: 11 (the only odd number)
+A - (initialize even variable, initialize odd variable)
+    let even = integers.filter(element => element % 2 === 0)
+    let odd = integers.filter(element => element % 2 !== 0)
 */
