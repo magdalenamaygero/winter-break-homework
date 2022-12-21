65f3bd4f686a10bf33f2e996eae92ee5ce8c66b7
magdalenamaygero <magdalenamgero@gmail.com>
2022-12-21 19:05:29 +0000

    second

--- code-wars-sotutions.js
+++ code-wars-sotutions.js
@@ -22,6 +22,29 @@ e: numbers below 10 that're multiples of 3 or 5: 3, 5, 6, 9
    
 a: input number. initialize sum. iterate through num. 
    if num is less than 0, return 0
-   if i%3 or i%5 == 0, increment the sum
+   if i%3 or i%5 == 0, increment the sum by that num
    return sum 
+*/
+
+function likes(names) {
+  return names.length === 0 ? "no one likes this" :
+  names.length === 1 ? `${names[0]} likes this` :
+  names.length === 2 ? `${names[0]} and ${names[1]} like this` :
+  names.length === 3 ? `${names[0]}, ${names[1]} and ${names[2]} like this` :
+  `${names[0]}, ${names[1]} and ${names.length - 2} others like this`
+}
+
+/*
+P - user inputs arary of names, we return string depending on the amount of names
+    if the amount of names is greater than 3, print 2 names and a sentence
+    
+E - ["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+    ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+    
+D - arrays, string
+A - if length = 0, return "no one likes this"
+    if length = 1,`${names[0]} likes this`
+    ... = 2, `${names[0]} and ${names[1]} like this`
+    ... = 3, `${names[0]}, ${names[1]} and ${names[2]} like this`
+    else :  = 4, `${names[0]}, ${names[1]} and ${names.length - 2} others like this` 
 */
